ce9dc3f759808ba87b440edb2a3cf59a9d8e88f6
SivolcC <23694346+SivolcC@users.noreply.github.com>
2020-02-06 13:32:45 +1100

    Added few comments to code, updated dataset

--- small_company.py
+++ small_company.py
@@ -1,27 +1,28 @@
 import anytree
 
+# dataset
 employees = [
     {"id": 100, "name": "Allan", "manager_id": 150},
     {"id": 220, "name": "Martin", "manager_id": 100},
     {"id": 150, "name": "Jamie", "manager_id": None},
     {"id": 275, "name": "Alex", "manager_id": 100},
     {"id": 400, "name": "Steve", "manager_id": 150},
-    {"id": 190, "name": "David", "manager_id": 400}
+    {"id": 190, "name": "David", "manager_id": 400},
+    {"id": 222, "name": "Henri", "manager_id": 190}
 ]
 
 
 def build_tree(p_num, p_node, data, nodes):
-    for c_num in nodes[p_num]:
+    for c_num in nodes[p_num]: # add children
         name = list(filter(lambda item: item['id'] == c_num, data))[0]['name']
-        #name = next((item['name'] for item in data if item['id'] == c_num), None)
         c_node = anytree.Node(name, parent=p_node)
-        if c_num in nodes:
+        if c_num in nodes: # goes to children
             build_tree(c_num, c_node, data, nodes)
 
             
 def find_root(data):
     try:
-        roots = list(filter(lambda item: not item['manager_id'], data))
+        roots = list(filter(lambda item: not item['manager_id'], data)) # get list of objects with no parent id
         if len(roots) < 1:
             raise Exception('Error in data: No root found.')
         elif len(roots) > 1:
@@ -34,17 +35,17 @@ def find_root(data):
 def main():
     try:
         parent_children = {}
-        for e in employees:
+        for e in employees: # map parents to list of children
             if e['manager_id'] in parent_children:
                 parent_children[e['manager_id']].append(e['id'])
             else:
                 parent_children[e['manager_id']] = [e['id']]
 
-        root_id, root_name = find_root(employees)
+        root_id, root_name = find_root(employees) # get root id and name
         tree = anytree.Node(root_name)
         build_tree(root_id, tree, employees, parent_children)
                 
-        for pre, fill, node in anytree.RenderTree(tree):
+        for pre, fill, node in anytree.RenderTree(tree): # render the tree
             print("{}{}".format(pre, node.name))
     except:
         raise
